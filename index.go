6750f077c78ffa36fe5dd932677b4acf67a2f4ee
root <root@localhost.localdomain>
2026-01-20 12:42:24 +0000

    Created a save function for the index (took a long time because i was learning encoding)

--- index.go
+++ index.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"encoding/gob"
+)
+
 type Index struct{
 	mu sync.RWMutex
 	index map[string][]int
@@ -18,4 +22,27 @@ func (idx *Index) AddDocument(doc Document){
 		}
 		idx.index[token] = append(ids, doc.ID)
 	}
-}  
+}
+
+func (idx *Index) Save(filePath string) error{
+	idx.mu.RLock()
+
+	defer idx.mu.RUnlock()
+
+	file, err := os.Create(filePath)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	if err := encoder.Encode(idx.index); err != nil{
+		return err
+	}
+
+	if err := encoder.Encode(idx.docStore); err != nil{
+		return err
+	}
+
+	return nil
+}
